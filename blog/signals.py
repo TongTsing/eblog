db4c55dc451564e87973cac2e3ebaf3e02eba5b8
tongqing <toneqing@163.com>
2025-02-01 00:34:21 +0800

    评论展示
    Signed-off-by: tongqing <toneqing@163.com>

--- blog/signals.py
+++ blog/signals.py
@@ -2,12 +2,12 @@
 # Author: Tongqing
 # Date Created: 2025/2/1
 from django.dispatch import receiver
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_save
 from .models import *
 from blog.views import logger
 
 
-@receiver(post_delete, sender=BlogComment)
+@receiver(post_save, sender=BlogComment)
 def cascade_logical_delete(sender, instance, **kwargs):
     if instance.is_delete:
         logger.info(f"Deleting {instance}")
