c49399aaf776f3863aeffead82c32018b84aa30e
tongqing <toneqing@163.com>
2025-02-01 02:17:49 +0800

    评论展示
    Signed-off-by: tongqing <toneqing@163.com>

--- blog/signals.py
+++ blog/signals.py
@@ -7,10 +7,17 @@ from .models import *
 from blog.views import logger
 
 
+def cascade_delete_replies(comment):
+    # 更新当前评论的所有回复
+    comment.replies.update(is_delete=True)
+
+    # 对所有回复的子回复也进行递归删除
+    for reply in comment.replies.all():
+        cascade_delete_replies(reply)
+
 @receiver(post_save, sender=BlogComment)
 def cascade_logical_delete(sender, instance, **kwargs):
     if instance and instance.is_delete:
         logger.info(f"Deleting {instance}")
-        # 获取与该评论相关的所有回复，并将它们标记为删除
-        instance.replies.update(is_delete=True)
+        cascade_delete_replies(instance)
 
