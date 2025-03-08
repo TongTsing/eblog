913e1e1a68d024e5bca979dce7c623bc92e47b56
tongqing <toneqing@163.com>
2025-03-08 18:23:22 +0800

    使用Django-cron定时更新访问计数到数据库中
    Signed-off-by: tongqing <toneqing@163.com>

--- blog/cron.py
+++ blog/cron.py
@@ -12,4 +12,4 @@ class BlogAccessCron(CronJobBase):
     def do(self):
         print("do")
         blogcount = BlogViewCountSingleton()
-        blogcount.save_to_database(blog_id=1)
+        print("blogcount.count", blogcount.get_blogview_count())
