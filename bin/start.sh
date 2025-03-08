a91cd720de5daa4913a09ad9a49d902f64593a8b
tongqing <toneqing@163.com>
2025-03-08 13:09:08 +0800

    新增项目启停脚本
    Signed-off-by: tongqing <toneqing@163.com>

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,1 @@
-python3 manage.py runserver 0.0.0.0:8181
+nohup gunicorn --bind 0.0.0.0:8181 eblog.wsgi     --access-logfile log/gunicorn/access.log     --error-logfile log/gunicorn/error.log     --pid runtime/gunicorn.pid --daemon
