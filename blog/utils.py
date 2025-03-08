eb129346e74bac79cc448bf53dfafd2b15cbc35d
tongqing <toneqing@163.com>
2025-03-08 16:43:04 +0800

    使用Django-cron定时更新访问计数到数据库中
    Signed-off-by: tongqing <toneqing@163.com>

--- blog/utils.py
+++ blog/utils.py
@@ -1,3 +1,4 @@
+import threading
 from os import access
 
 import redis
@@ -10,25 +11,26 @@ from blog_auth.views import logger
 class BlogViewCountSingleton(object):
     _instance = None
     _BlogViewCount = {}
+    _lock = threading.Lock()  # 用于线程安全
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super().__new__(
-               cls, *args, **kwargs
-            )
+            cls._instance = super().__new__(cls, *args, **kwargs)
         return cls._instance
 
     def get_blogview_count(self, blog_id):
-        return self._BlogViewCount.get(blog_id, 0)
+        with self._lock:
+            return self._BlogViewCount.get(blog_id, 0)
 
     def increment_blogview_count(self, blog_id):
-        if blog_id not in self._BlogViewCount:
-            self._BlogViewCount[blog_id] = 0
-        self._BlogViewCount[blog_id] += 1
+        with self._lock:
+            if blog_id not in self._BlogViewCount:
+                self._BlogViewCount[blog_id] = 0
+            self._BlogViewCount[blog_id] += 1
 
     def save_to_database(self, blog_id):
-        blogview_count = self.get_blogview_count(blog_id)
-
-        if blogview_count > 0:
-            Blog.objects.filter(id=blog_id).update(access_times=F("access_times")+blogview_count)
-            self._BlogViewCount[blog_id] = 0
+        with self._lock:
+            blogview_count = self.get_blogview_count(blog_id)
+            if blogview_count > 0:
+                Blog.objects.filter(id=blog_id).update(access_times=F("access_times")+blogview_count)
+                self._BlogViewCount[blog_id] = 0
--- blog/views.py
+++ blog/views.py
@@ -99,6 +99,7 @@ class blog_detail(View):
         # 增加访问计数
         blog_counter = BlogViewCountSingleton()
         blog_counter.increment_blogview_count(blog_id)
+        blog_counter.save_to_database(blog_id)
 
         # 获取评论树
         comment_tree = self.get_nested_comments(parent_comment=None, blog_id=blog_id)
