dea75ce950c7331aa8a1b0fbf955f226c79134b9
tongqing <toneqing@163.com>
2025-03-12 19:48:37 +0800

    使用Django-cron定时更新访问计数到数据库中
    Signed-off-by: tongqing <toneqing@163.com>

--- blog_auth/admin.py
+++ blog_auth/admin.py
@@ -8,6 +8,8 @@ from .models import User
 class EblogUserAdmin(UserAdmin):
     model = User
     list_display = ('email', 'username', 'is_superuser', 'is_staff', 'date_joined')
+    # 默认的user中是有first_name和second_name的，自定义user模型中如果删除了这两个字段，需要排除
+    exclude = ('first_name', 'last_name')
     # fieldsets = UserAdmin.fieldsets + (
     #     (None, {'fields': ('phone_number',)}),
     # )
